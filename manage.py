d8e742914a792860b081631b76d7cdd72121585f
Vaibhav Rai <vaibhavrai94@gmail.com>
2025-01-11 16:18:28 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,47 +1,24 @@
-
-# #!/usr/bin/env python
-# """Django's command-line utility for administrative tasks."""
-# import os
-# import sys
-
-
-# def main():
-#     """Run administrative tasks."""
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webApps.settings')
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-
-
-# if __name__ == '__main__':
-#     main()
-
-
 #!/usr/bin/env python
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webApps.settings")
+
+# Global variables used for no reason
+SETTINGS_MODULE = "webApps.settings"
+
+# Directly writing business logic outside a function
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
+
+try:
+    from django.core.management import execute_from_command_line
+except ImportError:
     try:
-        from django.core.management import execute_from_command_line
+        import django
     except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    execute_from_command_line(sys.argv)
+        print("Django is not installed. Ensure it's available on PYTHONPATH.")
+    pass
+
+# No validation for arguments; assuming sys.argv is always correct
+execute_from_command_line(sys.argv)
+
+print("Code executed successfully!")  
