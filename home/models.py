ca09f1e50a0a828cf660bbf14cc809767defbbcd
Vaibhav Rai <vaibhavrai94@gmail.com>
2025-03-28 12:55:14 +0530

    UNITRON Auto commit at 3/28/2025, 7:25:13 AM

--- home/models.py
+++ home/models.py
@@ -6,9 +6,3 @@ class contacts(models.Model):
     email = models.EmailField(max_length=30)
     phone=models.CharField(max_length=30)
     concern=models.TextField()
-    
-
-
-
-#python3 manage.py makemigrations after this
-#python3 manage.py migrate  (goes to db and creates query)
